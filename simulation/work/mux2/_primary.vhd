48e864d9f6b85c2d623904dc5a77977241192887
Aristotle <aristotle.martin@duke.edu>
2021-10-30 16:55:21 -0400

    added 2 to 1 mux and test bench

---
+++ simulation/work/mux2/_primary.vhd
@@ -0,0 +1,15 @@
+library verilog;
+use verilog.vl_types.all;
+entity mux2 is
+    generic(
+        DATA_WIDTH      : integer := 32
+    );
+    port(
+        Din0            : in     vl_logic_vector;
+        Din1            : in     vl_logic_vector;
+        \select\        : in     vl_logic;
+        Dout            : out    vl_logic_vector
+    );
+    attribute mti_svvh_generic_type : integer;
+    attribute mti_svvh_generic_type of DATA_WIDTH : constant is 1;
+end mux2;
