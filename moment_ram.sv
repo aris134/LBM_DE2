ca978fbbc9a7903e6280f443c9ab10ee9c6788af
Aristotle <aristotle.martin@duke.edu>
2021-10-30 15:20:39 -0400

    added moment ram module plus test bench

---
+++ moment_ram.sv
@@ -0,0 +1,19 @@
+module moment_ram #(DEPTH=16*16,
+		ADDRESS_WIDTH=$clog2(DEPTH),
+		DATA_WIDTH=32) (input logic [ADDRESS_WIDTH-1:0] address,
+							 input Clk,
+							 input WE,
+							 input logic signed [DATA_WIDTH-1:0] data_in,
+							 output logic signed [DATA_WIDTH-1:0] data_out);
+		
+		logic signed [DATA_WIDTH-1:0] d_out;
+		logic [DATA_WIDTH-1:0] mem [DEPTH-1:0];
+		assign data_out = mem[address];
+		
+		always @ (posedge Clk)
+		begin
+			if (WE) begin
+				mem[address] <= data_in;
+			end	
+		end
+endmodule
---
+++ tb_moment_ram.sv
@@ -0,0 +1,34 @@
+module tb_moment_ram();
+
+	timeunit 1ns;
+	timeprecision 1ns;
+	
+	parameter DEPTH = 16*16;
+	parameter ADDRESS_WIDTH = $clog2(DEPTH);
+	parameter DATA_WIDTH = 32;
+	parameter CLK_PERIOD = 20; // 50 MHz clock
+	
+	logic [ADDRESS_WIDTH-1:0] address;
+	logic Clk;
+	logic WE;
+	logic signed [DATA_WIDTH-1:0] data_in;
+	logic signed [DATA_WIDTH-1:0] data_out;
+	
+	moment_ram #(.DEPTH(DEPTH), .ADDRESS_WIDTH(ADDRESS_WIDTH), .DATA_WIDTH(DATA_WIDTH)) ram0 (.*);
+	
+	always #(CLK_PERIOD / 2) Clk = ~Clk;
+	
+	initial begin
+		Clk = 0;
+		WE = 0;
+		address = 8'h00;
+		
+		#10 data_in = 32'h1234_5678;
+		#12 WE = 1'b1;
+		#12 address = 8'h12;
+		#2	 data_in = 32'hABCC_CDEF;
+		#20 WE = 1'b0;
+		#10 $finish;
+	end
+
+endmodule
