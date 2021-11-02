e4895de1254cfe3ee144de054ae55be07cd37ae5
Aristotle <aristotle.martin@duke.edu>
2021-11-02 17:56:52 -0400

    additions to equilibrium calculations and boundary conditions

--- wall_detector.sv
+++ wall_detector.sv
@@ -3,6 +3,7 @@ module wall_detector #(GRID_DIM = 16*16, INIT_COUNT_WIDTH = $clog2(GRID_DIM), CO
 							  input logic [INIT_COUNT_WIDTH-1:0] x,
 							  input logic [COUNT_WIDTH-1:0] y,
 							  output logic LID, BOTTOM_WALL, LEFT_WALL, RIGHT_WALL);
+							  
 
 
 always_ff @ (negedge Reset or posedge Clear)	
