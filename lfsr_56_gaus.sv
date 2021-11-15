03a4853ae2f516f22588b07b5bb2c91fc028de43
Adam Krekorian <46938904+adamkrekorian@users.noreply.github.com>
2021-11-15 13:14:21 -0500

    updated lfsr_65_gaus

--- lfsr_56_gaus.sv
+++ lfsr_56_gaus.sv
@@ -4,39 +4,74 @@ module lfsr_56_gaus		// hard-coded to 56 here just to make it simple (would want
 				  input logic [55:0] seed,
 				  output logic [55:0] pseudo_rand_gaus);
 	
-    logic [55:0] d_out;
+    
+	logic [55:0] interm_out1;
+	logic [55:0] interm_out2;
+	logic [55:0] interm_out3;
+	logic [55:0] interm_out4;
+
+	logic [55:0] interm_out1s;
+	logic [55:0] interm_out2s;
+	logic [55:0] interm_out3s;
+	logic [55:0] interm_out4s;
+
+	logic [55:0] interm_out5;
+	logic [55:0] interm_out6;
+
+	logic [55:0] interm_out5s;
+	logic [55:0] interm_out6s;
+
+	logic [55:0] interm_out7;
+	logic [55:0] interm_out7s;
+
+	logic [55:0] d_out;
 	 assign pseudo_rand_gaus = d_out;
 	 
+	initial begin
 	 // based off https://ietresearch.onlinelibrary.wiley.com/doi/epdf/10.1049/el.2015.3418
-	 logic feedback1;
-	 logic feedback2;
-	 logic feedback3;
-	 logic feedback4;
+	 lfsr_56 (.Clk(Clk),
+		 		.Reset(Reset),
+				.seed(seed),
+				.pseudo_rand(interm_out1));
 
-	 assign feedback1 = (d_out[22] ~^ d_out[21]) ~^ (d_out[1] ~^ d_out[0]);
-	 assign feedback2 = (d_out[55] ~^ d_out[54]) ~^ (d_out[1] ~^ d_out[0]);
-	 assign feedback3 = (d_out[31] ~^ d_out[30]) ~^ (d_out[1] ~^ d_out[0]);
-	 assign feedback4 = (d_out[15] ~^ d_out[14]) ~^ (d_out[1] ~^ d_out[0]);
+	 lfsr_56 (.Clk(Clk),
+		 		.Reset(Reset),
+				.seed(seed),
+				.pseudo_rand(interm_out2));
 
-	 feedback1 = feedback1 >> 1;
-	 feedback2 = feedback2 >> 1;
-	 feedback3 = feedback3 >> 1;
-	 feedback4 = feedback4 >> 1;
+	 lfsr_56 (.Clk(Clk),
+		 		.Reset(Reset),
+				.seed(seed),
+				.pseudo_rand(interm_out3));
 
-	 feedback1 = feedback1 + feedback2;
-	 feedback3 = feedback3 + feedback4;
+	 lfsr_56 (.Clk(Clk),
+		 		.Reset(Reset),
+				.seed(seed),
+				.pseudo_rand(interm_out4));
 
-	 feedback1 = feedback1 >> 1;
-	 feedback3 = feedback3 >> 1;
+	 assign interm_out1s = interm_out1 >> 1;
+	 assign interm_out2s = interm_out2 >> 1;
+	 assign interm_out3s = interm_out3 >> 1;
+	 assign interm_out4s = interm_out4 >> 1;
 
-	 feedback1 = feedback1 >> 1;
+	 assign interm_out5 = interm_out1 + interm_out2; 
+	 assign interm_out6 = interm_out3 + interm_out4 ;
+
+	 assign interm_out5s = interm_out5 >> 1;
+	 assign interm_out6s = interm_out6 >> 1;
+
+	 assign interm_out7 = interm_out5 + interm_out6;
+
+	 assign interm_out7s = interm_out7 >> 1;
+
+	end
 	 
 	 always_ff @ (posedge Clk or negedge Reset)	
     begin
 	 	 if (~Reset)
 			  d_out <= seed;
 		 else
-			  d_out <= {feedback1, d_out[55:1]};
+			  d_out <= interm_out7s;
 	 end
 
 endmodule
